afedd1c5792e0b74e9ad9dbdd378b28aca0c75d9
Ghilain <ishimweghilain5050@gmail.com>
2023-01-10 11:29:08 +0200

    updates

--- uniquepaths.js
+++ uniquepaths.js
@@ -9,16 +9,16 @@
     const cachemap = Array.from({length: m}, e => Array(n));
     cachemap[m-1][n-1] = 1;
     
-    function cellCalc(x, y) {
+    function cellFind(x, y) {
         // if not in chache then calc
         if (!cachemap[x][y]) {
             cachemap[x][y] = 
                 // if out of map then + 0
-                (x < m - 1 ? cellCalc(x + 1, y) : 0) + 
-                (y < n - 1 ? cellCalc(x, y + 1) : 0);
+                (x < m - 1 ? cellFind(x + 1, y) : 0) + 
+                (y < n - 1 ? cellFind(x, y + 1) : 0);
         }
         return cachemap[x][y];
     }
     
-    return cellCalc(0, 0);
+    return cellFind(0, 0);
 };
