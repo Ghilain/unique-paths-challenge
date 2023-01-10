79c30c4af78e9a50a78009c30057fb55a391c23c
Ghilain <ishimweghilain5050@gmail.com>
2023-01-10 11:26:42 +0200

    variable to to generate map

--- uniquepaths.js
+++ uniquepaths.js
@@ -3,4 +3,10 @@
  * @param {number} n
  * @return {number}
  */
- 
+ var uniquePaths = function(m, n) {
+    
+    // generate map
+    const cachemap = Array.from({length: m}, e => Array(n));
+    cachemap[m-1][n-1] = 1;
+    
+};
